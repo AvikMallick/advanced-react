d6b0bb715eb5087c17e156c83039b76cbb0c00a5
Avik Mallick <avikmallick111101@gmail.com>
2023-02-20 21:30:03 +0530

    adding code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,6 +7,7 @@ const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <button onClick={props.onLogout}>Logout</button>
     </Card>
   );
 };
